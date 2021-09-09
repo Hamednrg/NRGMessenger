9743a1902226f736f06dec0de10d0cb2a42ca1f1
hamednrg <hamednajirg@gmail.com>
2021-09-09 13:20:04 +0430

    new viewcontrolers

---
+++ Messenger/Controllers/LoginViewController.swift
@@ -0,0 +1,14 @@
+//
+//  LoginViewController.swift
+//  Messenger
+//
+//  Created by Hamed on 6/18/1400 AP.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        
+    }
+}
---
+++ Messenger/Controllers/RegisterViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RegisterViewController.swift
+//  Messenger
+//
+//  Created by Hamed on 6/18/1400 AP.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+ 
+}
