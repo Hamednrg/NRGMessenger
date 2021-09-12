1b41c260cd10a2b9ab277f304934c72042bf1886
hamednrg <hamednajirg@gmail.com>
2021-09-12 22:55:27 +0430

    FireBase auth login and registration are added

--- Messenger/Controllers/Login/LoginViewController.swift
+++ Messenger/Controllers/Login/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
     
@@ -101,7 +102,14 @@ class LoginViewController: UIViewController {
         guard let email = emailField.text, let password = passwordField.text, !email.isEmpty, !password.isEmpty, password.count >= 6 else { alertUserLoginError()
             return }
         // FireBase log in
-        
+        FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+            guard let result = authResult, error == nil else {
+                print("error")
+                return
+            }
+            let user = result.user
+            print("logged in user: \(user)")
+        }
     }
     
     func alertUserLoginError(){
