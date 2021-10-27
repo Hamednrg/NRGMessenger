d4b758a7cb3de4b29e9abd1cfb5cac6df593fe1e
hamednrg <hamednajirg@gmail.com>
2021-10-28 01:27:33 +0330

    profile image bug is fixed

--- Messenger/Controllers/ProfileViewController.swift
+++ Messenger/Controllers/ProfileViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 import FirebaseAuth
 import FBSDKLoginKit
 import GoogleSignIn
+import SDWebImage
 
 class ProfileViewController: UIViewController {
     
@@ -41,10 +42,13 @@ class ProfileViewController: UIViewController {
         imageView.layer.borderWidth = 3
         imageView.layer.masksToBounds = true
         
-        StorageManager.shared.downloadURL(for: path) { [weak self] result in
+        StorageManager.shared.downloadURL(for: path) { result in
             switch result{
             case .success(let url):
-                self?.downloadImage(imageView: imageView, url: url)
+                DispatchQueue.main.async {
+                    imageView.sd_setImage(with: url, completed: nil)
+                }
+//                self?.downloadImage(imageView: imageView, url: url)
             case .failure(let error):
                 print("failed to get download url: \(error)")
             }
@@ -54,15 +58,15 @@ class ProfileViewController: UIViewController {
         headerView.addSubview(imageView)
         return headerView
     }
-    func downloadImage(imageView: UIImageView, url: URL){
-        URLSession.shared.dataTask(with: url) { data, _, error in
-            guard let data = data, error == nil else { return }
-            DispatchQueue.main.async {
-                let image = UIImage(data: data)
-                imageView.image = image
-            }
-        }.resume()
-    }
+//    func downloadImage(imageView: UIImageView, url: URL){
+//        URLSession.shared.dataTask(with: url) { data, _, error in
+//            guard let data = data, error == nil else { return }
+//            DispatchQueue.main.async {
+//                let image = UIImage(data: data)
+//                imageView.image = image
+//            }
+//        }.resume()
+//    }
     
 }
 
